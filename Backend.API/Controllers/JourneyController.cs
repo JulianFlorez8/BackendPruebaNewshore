df934aeced827f437335aa379141c383e16e1611
Julian F <julian.1701913963@ucaldas.edu.co>
2023-02-12 18:18:35 -0500

    Documentación métodos

--- Backend.API/Controllers/JourneyController.cs
+++ Backend.API/Controllers/JourneyController.cs
@@ -19,15 +19,23 @@ namespace Backend.API.Controllers
         private readonly FlightService flightService = new FlightService(new FlightRepository(BackContext.Create()));
         private double totalPrice = 0.0;
 
+        /**
+        * Metodo que Post de la API que recibe los parametros y llama a las de mas funciones para
+        * construir el objeto y dar respuesta a la petición
+        * @param inputParameters: parametros que serán recibidos
+        * @returns Task<IHttpActionResult> con el objeto en caso de ser exitoso
+        */
 
         [HttpPost]
         [Route("api/Journey/calculate")]
         public async Task<IHttpActionResult> Post(InputParameters inputParameters)
         {
             var flights = await flightService.GetAll();
-            Journey journey = new Journey();
-            journey.Origin = inputParameters.Origin;
-            journey.Destination = inputParameters.Destination;
+            Journey journey = new Journey
+            {
+                Origin = inputParameters.Origin,
+                Destination = inputParameters.Destination
+            };
             List<Flight> result = new List<Flight>();
             SearchFlightsRecursive(journey.Origin, journey.Destination, result, 0, flights, inputParameters);
             journey.Flights = result;
@@ -52,6 +60,15 @@ namespace Backend.API.Controllers
             return Ok(journey);
         }
 
+        /**
+        * Metodo que filtra los vuelos cuyo origin coincida con el origin entrante por parametro y despues 
+        * realiza llamadas recursivas con estos, añadiendo los vuelos a result hasta encontrar un vuelo cuyo destination coincida con 
+        * el destination entrante por parametro
+        * @param origin: origen del viaje, destination: destino del viaje, result: lista de vuelos con la que se satisfaga el origin y destination.
+        * numberOfFlights: número de vuelos en result flights: la lista de todos los vuelos.
+        * inputParameters: objeto que contiene los parámetros que llegan por la solicitud.
+        * @returns booleano que indica si ya se encontró una ruta con el origin y destination especificados
+        */
 
         public bool SearchFlightsRecursive(string origin, string destination, List<Flight> result, int numberOfFlights, IEnumerable<Flight> flights, InputParameters inputParameters)
         {
@@ -80,10 +97,16 @@ namespace Backend.API.Controllers
             return false;
         }
 
-        public double CalculateTotalPrice(List<Flight> result)
+
+        /**
+       * Metodo que calcula el precio total del viaje, recorriendo cada vuelo y sumando su precio
+       * @param flights: lista de vuelos
+       * @returns double con el precio total
+       */
+        public double CalculateTotalPrice(List<Flight> flights)
         {
             double totalPrice = 0.0;
-            foreach (var flight in result)
+            foreach (var flight in flights)
             {
                 totalPrice += flight.Price;
             }
